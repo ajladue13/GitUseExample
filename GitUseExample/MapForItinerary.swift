3c0781cdfdb738f6704021a7e114a37a931caa46
Alex LaDue <aj.ladue@gmail.com>
2017-10-29 22:55:36 -0400

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Alex LaDue on 10/29/17.
+//  Copyright © 2017 Alex LaDue. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary: NSObject {
+    //Code
+}
